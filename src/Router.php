0374ec46ae930441bd00234bf72fc310b75fdbc6
Daniel Pogodda <daniel.pogodda@transfermarkt.de>
2023-02-02 19:26:26 +0100

    add router

---
+++ src/Router.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chestnut\Router;
+
+use Chestnut\Router\Attributes\Route;
+use Psr\Container\ContainerInterface;
+use ReflectionClass;
+
+class Router
+{
+    private array $routes = [];
+
+    public function __construct(private Resolver $resolver)
+    {
+    }
+
+    public function register(string $requestMethod, string $route, callable|array $action): static
+    {
+        $this->routes[$requestMethod][$route] = $action;
+
+        return $this;
+    }
+
+    public function registerRoutesFromControllerAttributes(array $controllers): static
+    {
+        foreach ($controllers as $controller) {
+            $controllerReflection = new ReflectionClass($controller);
+
+            foreach ($controllerReflection->getMethods() as $method) {
+                $attributes = $method->getAttributes(Route::class);
+
+                foreach ($attributes as $attribute) {
+                    $route = $attribute->newInstance();
+
+                    $this->register($route->method, $route->path, [$controller, $method->getName()]);
+                }
+            }
+        }
+    }
+
+    public function get(string $route, callable|array $action): static
+    {
+        return $this->register('get', $route, $action);
+    }
+
+    public function post(string $route, callable|array $action): static
+    {
+        return $this->register('post', $route, $action);
+    }
+
+    public function routes(): array
+    {
+        return $this->routes;
+    }
+
+    public function resolve(string $requestUri, string $requestMethod)
+    {
+        return $this->resolver->resolve($requestUri, $requestMethod, $this->routes);
+    }
+}
