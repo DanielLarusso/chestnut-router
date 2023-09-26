c9baa185e901554063725087e0ef1dcf15a94635
Daniel Pogodda <daniel.pogodda@transfermarkt.de>
2023-09-26 12:14:09 +0200

    update

--- src/Attribute/Route.php
+++ src/Attribute/Route.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Chestnut\Router\Attribute;
 
 use Attribute;
+use Chestnut\Http\RequestMethod;
 
 #[Attribute]
 readonly class Route
 {
-    public function __construct(public string $path, public string $method = 'GET')
+    public function __construct(public string $path, public RequestMethod $method = RequestMethod::GET)
     {
 
     }
